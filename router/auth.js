6d327d22d5abf187c2b6b322d1d2733ce61453f7
cmsggg <minseck_choi@naver.com>
2021-10-25 13:18:49 +0000

    Change table structure of management page and database. Add delete button function

--- router/auth.js
+++ router/auth.js
@@ -1,27 +1,63 @@
-const path = require('path');
+const secretKey = "THISISOURSTAGE-STAGEUS";
 const router = require('express').Router();
-const { Client } = require('pg');
+const jwt = require('jsonwebtoken');
 
 // Insert register api
-router.post('', (req, res) => {
+router.post('/', (req, res) => {
 
     // request AJAX data
     let idValue = req.body.idValue;
     let pwValue = req.body.pwValue;
 
-    console.log(idValue);
-    console.log(pwValue);
+    // Init response data
+    const result = {
+        "success": true,
+        "message": "",
+        "token": null
+    }
 
+    if (idValue == "stageus" && pwValue == "stageus0104") {
+
+        const jwtToken = jwt.sign(
+            {
+                id: idValue
+            }, 
+            secretKey,
+            {
+                expiresIn: "30d",
+                issuer: "stageus"
+            }
+        )
+
+        result.success = true;
+        result.token = jwtToken;
+    } else {
+        result.message = "회원 정보가 잘못되었습니다."
+    }
+    
+    res.send(result);
+});
+
+// Insert register api
+router.post('/varify', (req, res) => {
 
     // Init response data
     const result = {
-        "success" : false
+        "success" : false,
+        "message": ""
     };
 
-    if (idValue == "stageus" && pwValue == "stageus0104") {
+    try {
+        req.decoded = jwt.verify(req.headers.auth, secretKey);
         result.success = true;
     }
-
+    catch(error) {
+        if (error.name == "TokenExpiredError") {
+            result.message = "만료된 로그인 토큰 입니다."
+        } else {
+            result.message = "유효하지 않은 로그인 토큰 입니다."
+        }
+    }
     res.send(result);
 });
 
--- router/register.js
+++ router/register.js
@@ -3,14 +3,16 @@ const router = require('express').Router();
 const { Client } = require('pg');
 
 // Insert register api
-router.post('', (req, res) => {
+router.post('/', (req, res) => {
 
     // request AJAX data
-    let nameValue = req.body.nameValue;
-    let contactValue = req.body.contactValue;
-    let jobValue = req.body.jobValue;
-    let optionValue = req.body.optionValue;
-    let subjectValue = req.body.subjectValue;
+    const nameValue = req.body.nameValue;
+    const contactValue = req.body.contactValue;
+    const jobValue = req.body.jobValue;
+    const optionValue = req.body.optionValue;
+    const subjectValue = req.body.subjectValue;
+    const generationValue = req.body.generationValue;
+    const durationValue = req.body.durationValue;
 
     // Init response data
     const result = {
@@ -31,8 +33,11 @@ router.post('', (req, res) => {
         password: "stageus0104",
         prot: 5432
     })
-    const query = "INSERT INTO homepage.register(name, contact, job, option, subject, register_date) VALUES($1, $2, $3, $4, $5, $6);";
-    const values = [nameValue, contactValue, jobValue, optionValue, subjectValue, newTime.toISOString()];
+    const query = "INSERT INTO homepage.register(name, contact, job, option, register_date, generation, duration, memo, subject) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);";
+    const values = [
+        nameValue, contactValue, jobValue, optionValue, newTime.toISOString(), 
+        generationValue, durationValue, "", subjectValue
+    ];
 
     pg.connect((err) => {
         if (err) {
@@ -51,7 +56,7 @@ router.post('', (req, res) => {
 });
 
 // Select register api
-router.get('', (req, res) => {
+router.get('/', (req, res) => {
 
     // Init response data
     const result = {
@@ -84,8 +89,12 @@ router.get('', (req, res) => {
                     res2.rows[index].contact,
                     res2.rows[index].job,
                     res2.rows[index].option,
+                    res2.rows[index].generation,
                     res2.rows[index].subject,
-                    res2.rows[index].register_date
+                    res2.rows[index].duration,
+                    res2.rows[index].register_date,
+                    res2.rows[index].memo,
+                    res2.rows[index].seq
                 ];
                 result.list.push(row);
             })
@@ -97,6 +106,47 @@ router.get('', (req, res) => {
         }
         pg.end();
     })
+});
+
+router.delete('/', (req, res) => {
+
+    // request AJAX data
+    const seqValue = req.body.seqValue;
+
+    // Init response data
+    const result = {
+        "success" : false
+    };
+
+    // Init psql account
+    const pg = new Client({
+        user: "ubuntu",
+        host: "localhost",
+        database: "stageus",
+        password: "stageus0104",
+        prot: 5432
+    })
+    const query = "DELETE FROM homepage.register WHERE seq = $1;";
+    const values = [seqValue];
+
+    pg.connect((err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    pg.query(query, values, (err, res2) => {
+        if (!err) {
+            result.success = true
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        pg.end();
+    })
+});
+
+router.put('/', (req, res) => {
+
 });
 
 module.exports = router;
