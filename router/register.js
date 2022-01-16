cf6aeb5b5c3ecb544d31a0828d517d45eb76713a
cmsggg <minseck_choi@naver.com>
2022-01-16 15:13:41 +0000

    UPDATE: register.js Change async method from promise and callback to async-await.

--- router/register.js
+++ router/register.js
@@ -2,7 +2,7 @@ const path = require('path');
 const router = require('express').Router();
 const { Client } = require('pg');
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
     // request AJAX data
     const nameValue = req.body.nameValue;
@@ -20,10 +20,8 @@ router.post('/', (req, res) => {
 
     // 강제로 빈 값을 입력했을 때 예외 처리
     if (nameValue == "" || contactValue == "" || jobValue == "" || optionValue == "" || subjectValue == "") {
-        console.log("error")
-        res.send(result);
-    }
-    else {
+        console.log("EmptyValueError: ", err);
+    } else {
 
         // Cal timestamp
         const today = new Date();
@@ -39,30 +37,30 @@ router.post('/', (req, res) => {
             password: "stageus0104",
             prot: 5432
         })
-        const query = "INSERT INTO homepage.register(name, contact, job, option, register_date, generation, duration, memo, subject) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);";
-        const values = [
-            nameValue, contactValue, jobValue, optionValue, newTime.toISOString(), 
-            generationValue, durationValue, "", subjectValue
-        ];
 
-        pg.connect((err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-        pg.query(query, values, (err, res2) => {
-            if (!err) {
+        const err = await pg.connect();
+        if (err) {
+            console.log("DBServerConnectionError: ", err);
+        } else {
+            const sql = "INSERT INTO homepage.register(name, contact, job, option, register_date, generation, duration, memo, subject) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);";
+            const values = [
+                nameValue, contactValue, jobValue, optionValue, newTime.toISOString(), 
+                generationValue, durationValue, "", subjectValue
+            ];
+
+            let err2, res2 = await pg.query(sql, values);
+            if (!err2) {
                 result.success = true
-                res.send(result);
             } else {
-                console.log(err);
+                console.log("SQLSyntaxError: ", err);
             }
-            pg.end();
-        })
+            await pg.end();
+        }
+        await res.send(result);
     }
 });
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 
     // Init response data
     const result = {
@@ -77,18 +75,17 @@ router.get('/', (req, res) => {
         password: "stageus0104",
         prot: 5432
     })
-    const query = "select * from homepage.register ORDER BY seq DESC";
 
-    pg.connect((err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-    pg.query(query, (err, res2) => {
-        if (!err) {
+    const err = await pg.connect();
+    if (err) {
+        console.log("ERR: ", err);
+    } else {
+        const sql = "select * from homepage.register ORDER BY seq DESC";
+        
+        let err2, res2 = await pg.query(sql);
+        if (!err2) {
 
             const rowList = res2.rows;
-
             rowList.forEach((elem, index) => {
                 const row = [
                     res2.rows[index].name,
@@ -104,17 +101,15 @@ router.get('/', (req, res) => {
                 ];
                 result.list.push(row);
             })
-
-            res.send(result);
-
         } else {
-            console.log(err);
+            console.log("ERR: ", err);
         }
-        pg.end();
-    })
+        await pg.end();
+    }
+    await res.send(result);
 });
 
-router.delete('/', (req, res) => {
+router.delete('/', async (req, res) => {
 
     // request AJAX data
     const seqValue = req.body.seqValue;
@@ -132,26 +127,26 @@ router.delete('/', (req, res) => {
         password: "stageus0104",
         prot: 5432
     })
-    const query = "DELETE FROM homepage.register WHERE seq = $1;";
-    const values = [seqValue];
 
-    pg.connect((err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-    pg.query(query, values, (err, res2) => {
-        if (!err) {
+    const err = await pg.connect()
+    if (err) {
+        console.log("ERR: ", err);
+    } else {
+        const sql = "DELETE FROM homepage.register WHERE seq = $1;";
+        const values = [seqValue];
+
+        let err2, res2 = await pg.query(sql, values);
+        if (!err2) {
             result.success = true
-            res.send(result);
         } else {
-            console.log(err);
+            console.log("ERR: ", err);
         }
-        pg.end();
-    })
+        await pg.end();
+    }
+    await res.send(result);
 });
 
-router.put('/', (req, res) => {
+router.put('/', async (req, res) => {
 
     // request AJAX data
     const nameValue = req.body.nameValue;
@@ -177,25 +172,25 @@ router.put('/', (req, res) => {
         password: "stageus0104",
         prot: 5432
     })
-    const query = "UPDATE homepage.register set name=$1, contact=$2, job=$3, option=$4, generation=$5, subject=$6, duration=$7, memo=$8 WHERE seq = $9;";
-    const values = [
-        nameValue, contactValue, jobValue, optionValue, generationValue, subjectValue, durationValue, memoValue, seqValue
-    ];
 
-    pg.connect((err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-    pg.query(query, values, (err, res2) => {
-        if (!err) {
+    const err = await pg.connect();
+    if (err) {
+        console.log("ERR: ", err);
+    } else {
+        const sql = "UPDATE homepage.register set name=$1, contact=$2, job=$3, option=$4, generation=$5, subject=$6, duration=$7, memo=$8 WHERE seq = $9;";
+        const values = [
+            nameValue, contactValue, jobValue, optionValue, generationValue, subjectValue, durationValue, memoValue, seqValue
+        ];
+
+        let err2, res2 = await pg.query(sql, values);
+        if (!err2) {
             result.success = true
-            res.send(result);
         } else {
-            console.log(err);
+            console.log("ERR: ", err);
         }
-        pg.end();
-    })
+        await pg.end();
+    }
+    await res.send(result);
 });
 
 module.exports = router;
