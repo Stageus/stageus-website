bf144093858362a0af1476f72923681c79c88a74
cmsggg <minseck_choi@naver.com>
2022-02-05 16:48:45 +0000

    UPDATE: all pages. Update all sytle code of several pages (register, faq, guide, refund)

--- public/js/initData.js
+++ public/js/initData.js
@@ -23,3 +23,28 @@ const img_link = document.getElementsByClassName("footer_link");
 [...img_link].forEach((item, index) => {
     item.href = footer_img[index];
 })
+
+// Navigation 열림 이벤트
+const menuOpenEvent = () => {
+    const target = document.getElementById("nav_menu_open");
+    if (window.innerWidth <= 409) {
+        target.style.width = "50%";
+        target.style.left = "50%";
+    } else if (window.innerWidth <= 767) {
+        target.style.width = "40%";
+        target.style.left = "60%";
+    } else if (window.innerWidth <= 1024) {
+        target.style.width = "30%";
+        target.style.left = "70%";
+    }
+    const cover = document.getElementById("nav_menu_cover");
+    cover.style.display = "block";
+}
+
+// Navigastion 닫힘 이벤트
+const menuCloseEvent = () => {
+    const target = document.getElementById("nav_menu_open");
+    target.style.left = "110%";
+    const cover = document.getElementById("nav_menu_cover");
+    cover.style.display = "none";
+}
